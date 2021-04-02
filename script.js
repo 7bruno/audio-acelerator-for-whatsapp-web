c5b3a6e3974ebee39a62365c486b0be41ab4a20e
Bruno Alexandre Gomes de Souza <souza.bruno.alexandre@gmail.com>
2021-04-02 01:11:33 -0300

    feat: audio acelerator for whatsapp web created, relase V0.0.1

---
+++ script.js
@@ -0,0 +1,48 @@
+const interval = setInterval(() => {
+  const header = document.querySelector("#side header");
+
+  if (header) {
+    clearInterval(interval);
+
+    const button = document.createElement("button");
+    button.id = "audio-speed-button";
+    button.innerHTML = "1 X";
+
+    button.addEventListener("click", () => {
+      actualSpeed = button.innerText.split(" ", 1)[0];
+      nextSpeed = parseFloat(actualSpeed) + 0.25;
+
+      const audios = document.querySelectorAll("audio");
+
+      if (nextSpeed < 3.0) {
+        audios.forEach((element) => {
+          element.playbackRate = nextSpeed;
+        });
+        button.innerHTML = nextSpeed + " X";
+      } else {
+        audios.forEach((element) => {
+          element.playbackRate = 1;
+        });
+        button.innerHTML = 1 + " X";
+      }
+    });
+
+    header.appendChild(button);
+
+    document.addEventListener("click", () => {
+      actualSpeed = parseFloat(button.innerText.split(" ", 1)[0]);
+      console.log("pane", actualSpeed);
+      const interval2 = setInterval(() => {
+        const audios = document.querySelectorAll("audio");
+        console.log(audios);
+        if (audios || interval2 == 2000) {
+          clearInterval(interval2);
+          audios.forEach((element) => {
+            element.playbackRate = actualSpeed;
+            console.log(element.playbackRate);
+          }, 50);
+        }
+      });
+    });
+  }
+}, 500);
